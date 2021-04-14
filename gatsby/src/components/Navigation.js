13d45edbbf6ed0a834f97bfe2a5bdc1ade2f4e76
Shannon Johnstone <shannon@shannonjohnstone.com.au>
2021-04-14 13:18:55 +1000

    Implement Nav extend layout and logo'

--- gatsby/src/components/Navigation.js
+++ gatsby/src/components/Navigation.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import styled from 'styled-components';
+import Logo from './Logo';
 
 const NavStyles = styled.nav`
   margin-bottom: 3rem;
+
+  .logo {
+    transform: translateY(-25%);
+  }
+
   ul {
     margin: 0;
     padding: 0;
@@ -12,6 +18,37 @@ const NavStyles = styled.nav`
     display: grid;
     grid-template-columns: 1fr 1fr auto 1fr 1fr;
     grid-gap: 2rem;
+  }
+
+  li {
+    --rotate: -2deg;
+    transform: rotate(var(--rotate));
+    order: 1;
+
+    &:nth-child(1) {
+      --rotate: 1deg;
+    }
+
+    &:nth-child(2) {
+      --rotate: -2.5deg;
+    }
+
+    &:nth-child(4) {
+      --rotate: 2.5deg;
+    }
+
+    &:hover {
+      --rotate: 3deg;
+    }
+  }
+
+  a {
+    font-size: 3rem;
+    text-decoration: none;
+
+    &:hover {
+      color: var(--red);
+    }
   }
 `;
 
@@ -26,7 +63,9 @@ export default function Navigation() {
           <Link to="/pizza/">Pizza Menu</Link>
         </li>
         <li>
-          <Link to="/">LOGO</Link>
+          <Link to="/">
+            <Logo />
+          </Link>
         </li>
         <li>
           <Link to="/slicemasters/">Slice masters</Link>
